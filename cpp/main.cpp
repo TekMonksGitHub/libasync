28d6409b1ead06761c598476462b5ce1ec8178a1
Rohit Kapoor <rvkapoor@tekmonks.com>
2019-10-30 14:29:46 +0800

    Cleanup - minor.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
     EventManager::start();                                      // start the event loop
     AsyncIO::start();                                           // start Async IO
     void pmain(vector<string>); pmain(args);                    // run the main program
-    while (!EventManager::isEventLoopShutdownRequested());       // program requested shutdown
+    while (!EventManager::isEventLoopShutdownRequested());      // program requested shutdown
     AsyncIO::stop();                                            // stop Async IO
     while (EventManager::areTherePendingEvents());              // wait for all events to be processed
     EventManager::stop();                                       // stop the event loop
